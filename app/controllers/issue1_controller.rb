0bb3d4395368ebccd8269e94fb2b30ecf32025c3
Lucas Oliveira <hub.lucasoliveira@gmail.com>
2026-06-14 15:33:09 -0300

    Fix Issue1 form

--- app/controllers/issue1_controller.rb
+++ app/controllers/issue1_controller.rb
@@ -1,13 +1,20 @@
 class Issue1Controller < ApplicationController
+  before_action :set_user, only: [ :new, :create ]
+
   def new
   end
 
   def create
-    if Current.user.check_issue1(params.expect([ :answer ]))
+    if @user.check_issue1(params.expect([ :answer ]))
       redirect_to board_path, notice: "Resposta correta!"
     else
       flash.now[:alert] = "Resposta incorreta. Tente novamente."
       render :new
     end
   end
+
+  private
+    def set_user
+      @user = Current.user
+    end
 end
