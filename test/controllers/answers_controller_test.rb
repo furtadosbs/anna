8dd677fad6a9640bda1234df10efa13fea5231c2
Lucas Oliveira <hub.lucasoliveira@gmail.com>
2026-06-21 17:22:02 -0300

    Add test

--- test/controllers/answers_controller_test.rb
+++ test/controllers/answers_controller_test.rb
@@ -4,4 +4,15 @@ class AnswersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
   end
+
+  test "post correct answers" do
+    sign_in_as(@user)
+    question = questions(:one)
+
+    assert_difference("Answer.count", 1) do
+      post question_answers_path(question), params: { answer: { value: question.correct_value } }
+    end
+
+    assert_redirected_to new_question_answer_path(question)
+  end
 end
