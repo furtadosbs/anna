1130af1ded63f24a47e38955cf98f9d37075211f
Lucas Oliveira <hub.lucasoliveira@gmail.com>
2026-09-20 15:04:00 -0300

    Remove new method from QuestionController

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,9 +2,4 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
-
-  def new
-    @question = Question.find(params[:id])
-    @answer = Answer.new
-  end
 end
